d57049b74e7af1030eb40106a9b489f191421d53
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-06 21:47:01 -0500

    Feat: Add route mutant

--- src/controllers/hackathon.controller.ts
+++ src/controllers/hackathon.controller.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from 'express';
 
 
-export const isMutant = (req: Request, res: Response) => {
+export const checkMutant = (req: Request, res: Response) => {
+
+  const { dna } = req.body;
+  if(!dna) {
+    return res.status(400).json({
+      message: 'DNA is required'
+    });
+  }
+
+  const isMutant = isMutantDNA(dna);
   return res.status(200).json({
-    ok: true,
-    message: 'Mutant'
+    isMutant
   });
+}
+
+const isMutantDNA = (dna: string[][]): boolean => {
+  const isMutant = false;
+  let sequenceCount = 0;
+  
+
+  if(sequenceCount >= 2) {
+    return true;
+  }
+  return isMutant;
 }
--- src/routes/hackathon.route.ts
+++ src/routes/hackathon.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { isMutant } from '../controllers/hackathon.controller';
+import { checkMutant } from '../controllers/hackathon.controller';
 
 const router = Router();
 
 // * POST {{url}}/mutant
-router.post('/mutant', isMutant);
+router.post('/mutant', checkMutant);
 
 export default router;
