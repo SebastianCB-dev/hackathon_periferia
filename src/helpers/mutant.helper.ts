d40b66da19623ed4e155db3576a501fd751b2077
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-07 13:30:04 -0500

    Feat: Update Function Errors

--- src/helpers/mutant.helper.ts
+++ src/helpers/mutant.helper.ts
@@ -20,32 +20,32 @@ export const isMutant = (dna: string[][]): boolean => {
 }
 
 // This function checks for sequences of 4 or more equal letters in the primary diagonal.
-const isMutantObliquePrimaryDiagonal = (dna: string[][], i: number, j: number): boolean => {
+export const isMutantObliquePrimaryDiagonal = (dna: string[][], i: number, j: number): boolean => {
   return j < dna[i].length - 3 &&
-    dna[i][j] === dna[i][j + 1] &&
-    dna[i][j] === dna[i][j + 2] &&
-    dna[i][j] === dna[i][j + 3];
+    dna[i][j] === dna[i + 1]?.[j + 1] &&
+    dna[i][j] === dna[i + 2]?.[j + 2] &&
+    dna[i][j] === dna[i + 3]?.[j + 3];
 }
 
 // This function checks for sequences of 4 or more equal letters in the secondary diagonal.
-const isMutantObliqueSecondaryDiagonal = (dna: string[][], i: number, j: number): boolean => {
+export const isMutantObliqueSecondaryDiagonal = (dna: string[][], i: number, j: number): boolean => {
   return i >= 3 &&
-    dna[i][j] === dna[i - 1][j] &&
-    dna[i][j] === dna[i - 2][j] &&
-    dna[i][j] === dna[i - 3][j];
+    dna[i][j] === dna[i - 1]?.[j - 1] &&
+    dna[i][j] === dna[i - 2]?.[j - 2] &&
+    dna[i][j] === dna[i - 3]?.[j - 3];
 }
 
 // This function checks for sequences of 4 or more equal letters in each column.
-const isMutantVertical = (dna: string[][], i: number, j: number): boolean => {
+export const isMutantVertical = (dna: string[][], i: number, j: number): boolean => {
   return i < dna.length - 3 &&
-    dna[i][j] === dna[i + 1][j] &&
-    dna[i][j] === dna[i + 2][j] &&
-    dna[i][j] === dna[i + 3][j];
+    dna[i][j] === dna[i + 1]?.[j] &&
+    dna[i][j] === dna[i + 2]?.[j] &&
+    dna[i][j] === dna[i + 3]?.[j];
 }
 
 // This function checks for sequences of 4 or more equal letters in each row
 // using a regular expression that matches 4 or more consecutive letters.
-const isMutantHorizontal = (dna: string[]): boolean => {
+export const isMutantHorizontal = (dna: string[]): boolean => {
   const regex: RegExp = /([A|T|C|G])\1{3,}/g;
   return dna.join('').match(regex) ? true : false;
 }
--- src/tests/unitary.test.ts
+++ src/tests/unitary.test.ts
@@ -1,5 +1,115 @@
-describe('Example', () => {
-  test('should pass', () => {
-    expect(1 + 1).toBe(2);
+// Unitare tests isMutant
+
+import { isMutant, isMutantHorizontal, isMutantObliquePrimaryDiagonal } from "../helpers/mutant.helper";
+
+describe('Mutant', () => {
+  it('should be mutant', () => {
+    const dna = [
+      'ATGCGA',
+      'CAGTGC',
+      'TTATGT',
+      'AGAAGG',
+      'CCCCTA',
+      'TCACTG'
+    ];
+    const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
+    const isMutantResult: boolean = isMutant(dnaParsed);
+    expect(isMutantResult).toBe(true);
+  });
+});
+
+describe('should not be mutant', () => {
+  it('should not be mutant', () => {
+    const dna = [
+      'ATGCGA',
+      'CAGTGC',
+      'TTATTT',
+      'AGACGG',
+      'GCGTCA',
+      'TCACTG'
+    ];
+    const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
+    const isMutantResult: boolean = isMutant(dnaParsed);
+    expect(isMutantResult).toBe(false);
+  });
+});
+
+describe('validator horizontal', () => {
+  it('should be mutant', () => {
+    const dna = [
+      'ATGCGA',
+      'CAGTGC',
+      'TTATTT',
+      'AGAAGG',
+      'CCCCTA',
+      'TCACTG'
+    ];
+    expect(isMutantHorizontal(dna[0].split(''))).toBe(false);
+    expect(isMutantHorizontal(dna[1].split(''))).toBe(false);
+    expect(isMutantHorizontal(dna[2].split(''))).toBe(false);
+    expect(isMutantHorizontal(dna[3].split(''))).toBe(false);
+    expect(isMutantHorizontal(dna[4].split(''))).toBe(true);
+    expect(isMutantHorizontal(dna[5].split(''))).toBe(false);
+  });
+});
+
+describe('validator oblique primary diagonal', () => {
+  it('should be mutant', () => {
+    const dna = [
+      'CCCCCC',
+      'DADDDD',
+      'DDADDD',
+      'DDDADD',
+      'DDDDAD',
+      'TCACTD'
+    ];
+    const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
+    expect(isMutantObliquePrimaryDiagonal(dnaParsed, 1, 1)).toBe(true);
+  });
+
+});
+
+describe('validator oblique primary diagonal', () => {
+  it('should not be mutant', () => {
+    const dna = [
+      'CCCCCC',
+      'DADDDD',
+      'DDADDD',
+      'DDDADD',
+      'DDDDDD',
+      'TCACTD'
+    ];
+    const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
+    expect(isMutantObliquePrimaryDiagonal(dnaParsed, 1, 1)).toBe(false);
+  });
+
+});
+
+describe('validator Horizontal', () => {
+
+  it('should be mutant', () => {
+    const dna = [
+      'CCCCCC',
+      'DADDDD',
+      'DDADDD',
+      'DDDADD',
+      'DDDDDD',
+      'TCACTD'
+    ];
+    const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
+    expect(isMutantHorizontal(dnaParsed[0])).toBe(true);
+  });
+
+  it('should not be mutant', () => {
+    const dna = [
+      'CCCCCC',
+      'DADDDD',
+      'FDADDD',
+      'DFDAFD',
+      'DDDDDC',
+      'TCACTD'
+    ];
+    const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
+    expect(isMutantHorizontal(dnaParsed[1])).toBe(false);
   });
 });
