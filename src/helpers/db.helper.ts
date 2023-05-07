fe8ea0ba004519178780e5aa0451cab6f7daa6e8
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-07 11:41:14 -0500

    Feat: Format Document

--- src/helpers/db.helper.ts
+++ src/helpers/db.helper.ts
@@ -1,15 +1,15 @@
 import { Mutant } from "../models/mutant";
 
 export const thereIsAMutant = async (dna: string[]) => {
-  
+
   const mutant = await Mutant.findOne({ dna: dna });
   return mutant;
-  
+
 }
 
 export const saveMutant = async (dna: string[], isMutant: boolean) => {
-    
-    const mutant = new Mutant({ dna: dna, isMutant: isMutant });
-    await mutant.save();
-    
+
+  const mutant = new Mutant({ dna: dna, isMutant: isMutant });
+  await mutant.save();
+
 }
