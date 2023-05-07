9c71b6ba846e829ddf0426d8e3eb05cc58aec95e
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-07 10:05:59 -0500

    Feat: Add Oblique and Vertical validators

--- src/controllers/hackathon.controller.ts
+++ src/controllers/hackathon.controller.ts
@@ -2,40 +2,52 @@ import { Request, Response } from 'express';
 
 
 export const checkMutant = (req: Request, res: Response) => {
-
   const { dna } = req.body;
-  if(!dna) {
+
+  if (!dna) {
     return res.status(400).json({
       message: 'DNA is required'
     });
   }
+
   const dnaParsed: string[][] = dna.map((dnaRow: string) => dnaRow.split(''));
-  const isMutant = isMutantDNA(dnaParsed);
-  return isMutant 
-         ? res.status(200).send(true) 
-         : res.status(403).send(false);
+  const result: boolean = isMutant(dnaParsed);
+  return result
+    ? res.status(200).send(true)
+    : res.status(403).send(false);
 }
 
-const isMutantDNA = (dna: string[][]): boolean => {
-  const isMutant = false;
-  let sequenceCount = 0;
-  for(let i = 0; i < dna.length; i++) {    
+const isMutant = (dna: string[][]): boolean => {
+  const isMutant: boolean = false;
+  let sequenceCount: number = 0;
+  for (let i = 0; i < dna.length; i++) {
     // Horizontal check
-    if(isMutantHorizontal(dna[i])) sequenceCount++;
+    if (isMutantHorizontal(dna[i])) sequenceCount++;
 
-    for(let j = 0; j < dna[i].length; j++) {
-      
+    for (let j = 0; j < dna[i].length; j++) {
+      if(j < dna[i].length - 3 && 
+         dna[i][j] === dna[i][j + 1] && 
+         dna[i][j] === dna[i][j + 2] && 
+         dna[i][j] === dna[i][j + 3]) {
+        sequenceCount++;
+      }
+      if(i === 3 && 
+         dna[i][j] === dna[i - 1][j] && 
+         dna[i][j] === dna[i - 2][j] && 
+         dna[i][j] === dna[i - 3][j]) {
+        sequenceCount++;
+      }
     }
   }
   console.log(sequenceCount);
-  if(sequenceCount >= 2) {
+  if (sequenceCount >= 2) {
     return true;
   }
   return isMutant;
 }
 
-const isMutantHorizontal = (dna: string[]): boolean => {  
+const isMutantHorizontal = (dna: string[]): boolean => {
   // *This regex matches 4 or more consecutive letters  
-  const regex = /([A|T|C|G])\1{3,}/g;
+  const regex: RegExp = /([A|T|C|G])\1{3,}/g;
   return dna.join('').match(regex) ? true : false;
 }
